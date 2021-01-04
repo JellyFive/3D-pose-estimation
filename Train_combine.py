eca7e7c8844e8a6580c0e72ae678fc0791361d30
JellyFive <wzn0408@163.com>
2021-01-04 19:02:50 +0800

    update

---
+++ Train_combine.py
@@ -0,0 +1,163 @@
+from torch_lib.dataset_posenet import *
+from torch_lib.posenet_combine import Model, OrientationLoss, FocalLoss
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision.models import vgg, resnet, densenet, mobilenet
+from torch.utils import data
+
+from tensorboardX import SummaryWriter
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+def main():
+
+    # hyper parameters
+    epochs = 200
+    batch_size = 8
+    w = 1
+    alpha = 1
+
+    print("Loading all detected objects in dataset...")
+
+    train_path = "/media/lab/TOSHIBAEXT/BuildingData/training"
+    dataset = Dataset(train_path)  # 自定义的数据集
+
+    params = {"batch_size": batch_size, "shuffle": True, "num_workers": 6}
+
+    generator = data.DataLoader(dataset, **params)  # 读取Dataset中的数据
+
+    base_model = mobilenet.mobilenet_v2(pretrained=True)  # 加载模型并设置为预训练模式
+    model = Model(features=base_model).cuda()
+    print(model)
+
+    opt_SGD = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
+    conf_loss_func = nn.CrossEntropyLoss().cuda()
+    orient_loss_func = OrientationLoss
+    criterion = nn.SmoothL1Loss(reduction='mean').cuda()
+
+    # load any previous weights
+    model_path = ("/home/lab/Desktop/wzndeep/posenet-build--eular/weights/")
+    latest_model = None
+    first_epoch = 0
+    if not os.path.isdir(model_path):
+        os.mkdir(model_path)
+    else:
+        try:
+            latest_model = [
+                x for x in sorted(os.listdir(model_path)) if x.endswith(".pkl")
+            ][-1]
+        except:
+            pass
+
+    if latest_model is not None:
+        # 解序列化一个pickled对象并加载到内存中
+        checkpoint = torch.load(model_path + latest_model)
+        # 加载一个state_dict对象，加载模型用于训练或验证
+        model.load_state_dict(checkpoint["model_state_dict"])
+        opt_SGD.load_state_dict(checkpoint["optimizer_state_dict"])  # 同上
+        first_epoch = checkpoint["epoch"]
+        loss = checkpoint["loss"]
+
+        print("Found previous checkpoint: %s at epoch %s" %
+              (latest_model, first_epoch))
+        print("Resuming training....")
+
+    # 训练网络
+
+    total_num_batches = int(len(dataset) / batch_size)
+
+    writer = SummaryWriter(
+        "/home/lab/Desktop/wzndeep/posenet-build--eular/runs/")
+
+    for epoch in range(first_epoch + 1, epochs + 1):  # 多批次循环
+        curr_batch = 0
+        passes = 0
+        for local_batch, local_labels in generator:  # 获取输入数据
+
+            truth_orient_patch = local_labels["Orientation_patch"].float(
+            ).cuda()
+            truth_conf_patch = local_labels["Confidence_patch"].long().cuda()
+            truth_orient_yaw = local_labels["Orientation_yaw"].float().cuda()
+            truth_conf_yaw = local_labels["Confidence_yaw"].long().cuda()
+            truth_location = local_labels["Location"].float().cuda()
+
+            local_batch = local_batch.float().cuda()
+            [orient_patch, conf_patch, orient_yaw,
+             conf_yaw, location] = model(local_batch)
+
+            orient_patch_loss = orient_loss_func(orient_patch,
+                                                 truth_orient_patch,
+                                                 truth_conf_patch)
+
+            # softmax函数的输出值进行操作，每行——>返回每行最大值的索引
+            truth_conf_patch = torch.max(truth_conf_patch, dim=1)[1]
+            conf_patch_loss = conf_loss_func(conf_patch, truth_conf_patch)
+
+            loss_patch = conf_patch_loss + w * orient_patch_loss
+
+            orient_yaw_loss = orient_loss_func(orient_yaw, truth_orient_yaw,
+                                               truth_conf_yaw)
+
+            # softmax函数的输出值进行操作，每行——>返回每行最大值的索引
+            truth_conf_yaw = torch.max(truth_conf_yaw, dim=1)[1]
+            conf_yaw_loss = conf_loss_func(conf_yaw, truth_conf_yaw)
+
+            loss_yaw = conf_yaw_loss + w * orient_yaw_loss
+
+            loss_trans = criterion(location, truth_location)
+
+            total_loss = loss_patch + loss_yaw + alpha * loss_trans
+
+            opt_SGD.zero_grad()  # 梯度置0
+            total_loss.backward()  # 反向传播
+            opt_SGD.step()  # 优化
+
+            if passes % 10 == 0:  # 10轮显示一次，打印状态信息
+                print(
+                    "--- epoch %s | batch %s/%s --- [loss_patch: %s] | [loss_yaw: %s] | [loss_trans: %s] | [total_loss: %s]"
+                    % (
+                        epoch,
+                        curr_batch,
+                        total_num_batches,
+                        loss_patch.item(),
+                        loss_yaw.item(),
+                        loss_trans.item(),
+                        total_loss.item(),
+                    ))
+                passes = 0
+
+            passes += 1
+            curr_batch += 1
+        writer.add_scalar("loss_total", total_loss, epoch)
+        writer.add_scalar("loss_patch", loss_patch, epoch)
+        writer.add_scalar("orient_patch_loss", orient_patch_loss, epoch)
+        writer.add_scalar("conf_patch_loss", conf_patch_loss, epoch)
+        writer.add_scalar("loss_trans", loss_trans, epoch)
+
+        # save after every 10 epochs
+        if epoch % 20 == 0:
+            name = model_path + "epoch_%s.pkl" % epoch
+            print("====================")
+            print("Done with epoch %s!" % epoch)
+            print("Saving weights as %s ..." % name)
+            torch.save(
+                {
+                    "epoch": epoch,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": opt_SGD.state_dict(),
+                    "loss": total_loss,
+                },
+                name,
+            )
+            print("====================")
+
+    writer.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ library/evaluate.py
@@ -0,0 +1,131 @@
+import numpy as np
+import math
+# from pyquaternion import Quaternion
+from library.Math import get_corners
+
+
+def iou(gt_box, est_box):
+    xA = max(gt_box[0], est_box[0])
+    yA = max(gt_box[1], est_box[1])
+    xB = min(gt_box[2], est_box[2])
+    yB = min(gt_box[3], est_box[3])
+
+    if xB <= xA or yB <= yA:
+        return 0.
+
+    interArea = (xB - xA) * (yB - yA)
+
+    boxAArea = (gt_box[2] - gt_box[0]) * (gt_box[3] - gt_box[1])
+    boxBArea = (est_box[2] - est_box[0]) * (est_box[3] - est_box[1])
+
+    return interArea / float(boxAArea + boxBArea - interArea)
+
+
+def trans_error(gt_trans, est_trans):
+    """
+    :param gt_trans: 真实的三维坐标，np.array([x,y,z])
+    :param est_trans: 预测的三维坐标
+    :return trans_err_normL: 平方和误差
+    :return trans_err_single: x,y,z方向分别的误差
+    """
+    # L2范式，平方和
+    trans_err_norm = np.linalg.norm(gt_trans - est_trans)
+    # 绝对值
+    trans_err_single = np.abs(gt_trans - est_trans)
+
+    return trans_err_norm, trans_err_single
+
+
+def rot_error(gt_rot, est_rot):
+    """
+    :param gt_rot: 真实的旋转角度，np.array([patch,yaw,roll])
+    :param est_rot: 预测的旋转角度
+    :return rot_error = 2 * arccos(|gt_pose, est_pose|) * 180 / 3.14, 反余弦距离
+    """
+
+    # 将欧拉角转换为四元数
+    def eulerAnglesToQu(theta):
+
+        q = np.array([
+            math.cos(theta[0] / 2) * math.cos(theta[1] / 2) *
+            math.cos(theta[2] / 2) + math.sin(theta[0] / 2) *
+            math.sin(theta[1] / 2) * math.sin(theta[2] / 2),
+            math.sin(theta[0] / 2) * math.cos(theta[1] / 2) *
+            math.cos(theta[2] / 2) - math.cos(theta[0] / 2) *
+            math.sin(theta[1] / 2) * math.sin(theta[2] / 2),
+            math.cos(theta[0] / 2) * math.sin(theta[1] / 2) *
+            math.cos(theta[2] / 2) + math.sin(theta[0] / 2) *
+            math.cos(theta[1] / 2) * math.sin(theta[2] / 2),
+            math.cos(theta[0] / 2) * math.cos(theta[1] / 2) *
+            math.sin(theta[2] / 2) - math.sin(theta[0] / 2) *
+            math.sin(theta[1] / 2) * math.cos(theta[2] / 2)
+        ])
+        return q
+
+    gt_quat = eulerAnglesToQu(gt_rot)
+    est_quat = eulerAnglesToQu(est_rot)
+
+    ans = np.dot(gt_quat, est_quat.T)
+
+    return np.rad2deg(2 * math.acos(np.abs(ans)))
+
+    # 与上述等价
+    # gt_quat = Quaternion(eulerAnglesToQu(gt_rot))
+    # est_quat = Quaternion(eulerAnglesToQu(est_rot))
+
+    # return np.abs((gt_quat * est_quat.inverse).degrees)
+
+
+def add_err(dim, gt_trans, est_trans, gt_rot, est_rot):
+    """
+    :param dim:目标的尺寸
+    :param gt_trans, gt_rot: 真实的位姿
+    :param est_rot, est_rot: 预测的位姿
+    :return add_error = 8个二维顶点的欧氏距离的平均值
+    """
+    gt_corners_3D = get_corners(dim, gt_trans, gt_rot[0], gt_rot[1], gt_rot[2])
+    est_corners_3D = get_corners(dim, est_trans, est_rot[0], est_rot[1],
+                                 est_rot[2])
+
+    add_error = np.mean(np.linalg.norm(gt_corners_3D - est_corners_3D, axis=1))
+
+    return add_error
+
+
+if __name__ == "__main__":
+    trans_errors_norm = []
+    trans_errors_single = []
+    rot_errors = []
+    adds = []
+
+    gt_bbox = np.array([120, 200, 400, 700])
+    est_bbox = np.array([120, 200, 400, 650])
+
+    dim = np.array([2, 2, 2])
+
+    gt_trans = np.array([1, 2, 3])
+    est_trans = np.array([1, 2.2, 3.5])
+
+    gt_rot = np.array([0.5237, -0.5237, 0])
+    est_rot = np.array([0.5237, -0.5537, 0])
+
+    if iou(gt_bbox, est_bbox) >= 0.5:
+
+        trans_error = trans_error(gt_trans, est_trans)
+        trans_errors_norm.append(trans_error[0])
+        trans_errors_single.append(trans_error[1])
+        rot_errors.append(rot_error(gt_rot, est_rot))
+        adds.append(add_err(dim, gt_trans, est_trans, gt_rot, est_rot))
+
+    mean_trans_error_norm = np.mean(trans_errors_norm)
+    mean_trans_error_single = np.mean(trans_errors_single, axis=0)
+    mean_rot_error = np.mean(rot_errors)
+    mean_add = np.mean(adds)
+
+    
+    print("\tMean Rotation Error: {:.3f}".format(mean_rot_error))
+    print("\tMean Trans Error Norm: {:.3f}".format(mean_trans_error_norm))
+    print("\tMean Trans Errors: X: {:.3f}, Y: {:.3f}, Z: {:.3f}".format(
+        mean_trans_error_single[0], mean_trans_error_single[1],
+        mean_trans_error_single[2]))
+    print("\tMean ADD: {:.3f}".format(mean_add))
